c646b0ddff21d30c0ac4578cf010b086caedb5b7
Evan Palmer <evjpalmer@gmail.com>
2016-05-04 11:20:18 -0700

    add projects models, update settings, add THUMBNAIL_DIR

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'homepage',
+    'projects',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -120,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+THUMBNAIL_DIR = '{}/projects/static/projects/images/'.format(BASE_DIR)
--- portfolio/projects/models.py
+++ portfolio/projects/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from portfolio.settings import THUMBNAIL_DIR
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=64)
+    slug = models.SlugField()
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=64)
+    slug = models.SlugField()
+    tags = models.ManyToManyField(Tag, related_name='projects', blank=True)
+    short_description = models.CharField(
+        max_length=255, default="No short description provided")
+    long_description = models.TextField(default="No long description provided")
+    gist_url = models.URLField()
+    repo_url = models.URLField()
+    site_url = models.URLField(blank=True)
+    thumbnail = models.FilePathField(path=THUMBNAIL_DIR)
