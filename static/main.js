70502c02da57eafdc5a614e91f70f7217d5ca613
Evan Palmer <evjpalmer@gmail.com>
2016-05-03 18:58:51 -0700

    remove old static dir, start new scss stylesheet

--- static/main.js
+++
@@ -1,7 +0,0 @@
-// requires jQuery
-
-// code to manage the 'active' class on nav links 
-$(".nav a").on("click", function(){
-   $(".nav").find(".active").removeClass("active");
-   $(this).parent().addClass("active");
-});
