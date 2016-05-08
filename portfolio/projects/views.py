9046fb833e3974ae8875554b08e98a6bf97c8915
Evan Palmer <evjpalmer@gmail.com>
2016-05-07 23:20:08 -0700

    render thumbnail in project detail template

--- portfolio/projects/views.py
+++ portfolio/projects/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from portfolio.settings import MEDIA_URL, MEDIA_ROOT
 
 from .models import Project, Tag
 
@@ -6,14 +7,29 @@ from .models import Project, Tag
 def project_list(request):
     """Render the list of all projects"""
     projects = Project.objects.all()
-    return render(request, 'projects/project-list.html', {"projects": projects})
+    return render(
+        request,
+        'projects/project-list.html',
+        {
+            "projects": projects,
+            "media_url": MEDIA_URL,
+            "media_root": MEDIA_ROOT
+        }
+    )
 
 
 def project_detail(request, slug):
     """Render the detail page for the project matching slug"""
     project = Project.objects.get(slug=slug)
     return render(
-        request, 'projects/project-detail.html', {"project": project})
+        request,
+        'projects/project-detail.html',
+        {
+            "project": project,
+            "media_url": MEDIA_URL,
+            "media_root": MEDIA_ROOT
+        }
+    )
 
 
 def tag_list(request):
