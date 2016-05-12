48b29a8adcb7e1785dbacee871bf5b69cae63db8
Evan Palmer <evjpalmer@gmail.com>
2016-05-11 20:00:59 -0700

    register Snippet in admin

--- portfolio/projects/admin.py
+++ portfolio/projects/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from projects.models import Project, Tag
+from projects.models import Project, Tag, Snippet
 
 admin.site.register(Project)
 admin.site.register(Tag)
+admin.site.register(Snippet)
