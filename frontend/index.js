8b5e43baa75f7e365b8f67aa7b2f2a29974feadd
Evan Palmer <evjpalmer@gmail.com>
2016-07-11 21:06:11 -0700

    add route for PortfolioItemDetail

--- frontend/index.js
+++ frontend/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom'
 import { Router, Route, IndexRoute, hashHistory } from 'react-router'
 import CoreLayout from './src/components/CoreLayout/CoreLayout.js'
 import Main from './src/components/Main/Main.js'
+import PortfolioItemDetail from './src/components/PortfolioItemDetail/PortfolioItemDetail.js'
 
 ReactDOM.render(
   (<Router history={hashHistory}>
     <Route path="/" component={CoreLayout}>
       <IndexRoute component={Main} />
     </Route>
+    <Route path="/detail/:itemSlug" component={PortfolioItemDetail}/>
   </Router>),
   document.getElementById('app'))
